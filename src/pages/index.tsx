fe689ff0f93bd68cdebd111a60e20b1f8e0ecbb9
limahugovt <lima.hugo@escolar.ifrn.edu.br>
2025-03-16 11:50:32 -0300

    :tada: feat: Add responsive Header component with framer-motion

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,11 @@
-import HeaderMobile from "../components/HeaderMobile";
+import Header from "../components/Header";
 
 export default function Home() {
   return (
     <div className="w-full h-screen sm:p-[70px] p-4">
       <div className="bg-slate-900 w-full h-full rounded-lg border border-slate-700">
-        <HeaderMobile />
-        <p className="m-0">oi</p>
+        <Header />
+        <p className="m-0">oaasaksBDJKi</p>
       </div>
     </div>
   );
