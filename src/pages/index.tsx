30e20ba3e86a311b58894054d3fed30a24a9b50c
limahugovt <lima.hugo@escolar.ifrn.edu.br>
2025-03-15 22:59:46 -0300

    :tada: feat: Add Tailwind CSS setup

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="w-full h-full p-[70px]">
+      <div className="bg-black">
+        <p className="m-0">oi</p>
+      </div>
+    </div>
+  );
+}
